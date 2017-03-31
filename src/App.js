e96be18e717aefeefb8ab76eac9eeb1317ef654f
Andreas Sahle <andreas.sahle@wunderkraut.com>
2017-03-31 12:29:53 +0200

    rework App component to fix test

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import logo from './logo.svg';
 import './App.css';
 
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <AppBar
-          title="Title"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
-        />
-        <p className="App-intro">
-          save to reload.
-        </p>
-      </div>
+    return ( 
+      <MuiThemeProvider>
+        <div className="App">
+          <AppBar
+            title="Title"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+          />
+          <p className="App-intro">
+            save to reload.
+          </p>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,19 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-const Root = () => (
-  <MuiThemeProvider>
-    <App />
-  </MuiThemeProvider>
-);
 ReactDOM.render(
-  <Root />,
+  <App />,
   document.getElementById('root')
 );
