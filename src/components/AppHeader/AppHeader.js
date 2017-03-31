cdb4940efcd744a4ee0a5f2d5337ea8ff3b119cc
Andreas Sahle <andreas.sahle@wunderkraut.com>
2017-03-31 17:09:52 +0200

    add AppBar also to drawer

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
+import IconButton from 'material-ui/IconButton';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import MenuItem from 'material-ui/MenuItem';
 
 class AppHeader extends Component {
@@ -26,6 +28,11 @@ class AppHeader extends Component {
           open={this.state.open}
           onRequestChange={(open) => this.setState({open})}
         >
+          <AppBar
+            title=""
+            iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+            onLeftIconButtonTouchTap={this.handleToggle}
+          />
           <MenuItem onTouchTap={this.handleClose}>Section 01</MenuItem>
           <MenuItem onTouchTap={this.handleClose}>Section 02</MenuItem>
         </Drawer>
