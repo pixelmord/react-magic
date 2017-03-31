84d289ca5e108b5e01df3b7196a4e136466af257
Andreas Sahle <andreas.sahle@wunderkraut.com>
2017-03-31 11:15:12 +0200

    replace demo header with appbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,10 +7,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+        <AppBar
+          title="Title"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
         <p className="App-intro">
           save to reload.
         </p>
